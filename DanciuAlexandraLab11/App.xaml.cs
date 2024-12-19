6aebc2e57222596680bc3d20282f2e771eccb21a
ALEXANDRA-IOANA DANCIU <alexandra.ioana.danciu@stud.ubbcluj.ro>
2024-12-19 19:03:16 +0200

    Add project files.

---
+++ DanciuAlexandraLab11/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using DanciuAlexandraLab11.Data;
+namespace DanciuAlexandraLab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            //InitializeComponent();
+
+           // MainPage = new AppShell();
+
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ DanciuAlexandraLab11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DanciuAlexandraLab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
